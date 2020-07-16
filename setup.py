656390808fe646a13f2c0f80a41277f87933087f
matteoferla <matteo.ferla@gmail.com>
2020-07-16 17:32:54 +0100

    :hammer: alteration to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,34 @@
-from setuptools import setup
+########################################################################################################################
+__doc__ = \
+    """
+    Pip install file. All the files have this block here, so in PyCharm I can change them en-mass.
+    """
+
+__author__ = "Matteo Ferla. [Github](https://github.com/matteoferla)"
+__email__ = "matteo.ferla@gmail.com"
+__date__ = "10 July 2020 A.D."
+__license__ = "MIT"
+__version__ = "1.1.0"
+__citation__ = "None."
+
+########################################################################################################################
+
+
+from setuptools import setup, find_packages
+
+import os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='michelanglo_api',
-    version='1',
-    packages=['michelanglo_api'],
-    url='michelanglo.sgc.ox.ac.uk',
-    license='MIT',
-    author='Matteo Ferla',
-    author_email='matteo.ferla@gmail.com',
+    version= __version__,
+    packages=find_packages(),
+    url='https://github.com/matteoferla/MichelaNGLo-api',
+    license=__license__,
+    author=__author__,
+    author_email=__email__ ,
     description='API to interface with Michelanglo'
 )
